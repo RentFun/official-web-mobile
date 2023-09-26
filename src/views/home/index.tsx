4f0a6f5c33ba8e125bb6e9f51420c8837d54ba5a
kaelnew <kevin@rentfun.io>
2023-09-26 17:55:26 +0800

    subtitle updated

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -108,7 +108,7 @@ const TSectionScreen = (forwardRef((props:screenPropsType, ref:any) => {
             <div className="t-sun">
                 <img className="t-logo-center" src={require("./images/screen/logo.png")} alt=""/>
                 <div style={{ width: `${clientWidth}px` }} className="t-title">Liquidity for <br/> NFT Utility</div>
-                <div style={{ width: `${clientWidth}px` }} className="t-title-sub">Rent Original NFTs Through Access Delegation on Arbitrum</div>
+                <div style={{ width: `${clientWidth}px` }} className="t-title-sub">NFT Rental Protocol For Web3 Games</div>
                 <div className="t-icon">
                     <img onClick={openUrl.bind(this, 'https://twitter.com/rentfun_io')} src={require("./images/screen/3.png")} alt=""/>
                     <img onClick={openUrl.bind(this, 'https://github.com/RentFun')}  src={require("./images/screen/2.png")} alt=""/>
